dd432c4fd3c8530b6b9531fa78a2587dd2fde178
aokseen <lhl383307315@sina.com>
2025-11-01 21:34:56 +0800

    Upload some codes
    Upload some codes

---
+++ utils/utils.py
@@ -0,0 +1,76 @@
+import random
+
+import numpy as np
+import torch
+from PIL import Image
+
+
+
+def cvtColor(image):
+    if len(np.shape(image)) == 3 and np.shape(image)[2] == 3:
+        return image 
+    else:
+        image = image.convert('RGB')
+        return image 
+
+
+def resize_image(image, size):
+    w, h    = size
+    new_image = image.resize((w, h), Image.BICUBIC)
+    return new_image
+
+
+def get_classes(classes_path):
+    with open(classes_path, encoding='utf-8') as f:
+        class_names = f.readlines()
+    class_names = [c.strip() for c in class_names]
+    return class_names, len(class_names)
+
+def preprocess_input(image):
+    mean    = (123.68, 116.78, 103.94)
+    std     = (58.40, 57.12, 57.38)
+    image   = (image - mean)/std
+    return image
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group['lr']
+
+
+def seed_everything(seed=11):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def worker_init_fn(worker_id, rank, seed):
+    worker_seed = rank + seed
+    random.seed(worker_seed)
+    np.random.seed(worker_seed)
+    torch.manual_seed(worker_seed)
+
+def show_config(**kwargs):
+    print('Configurations:')
+    print('-' * 70)
+    print('|%25s | %40s|' % ('keys', 'values'))
+    print('-' * 70)
+    for key, value in kwargs.items():
+        print('|%25s | %40s|' % (str(key), str(value)))
+    print('-' * 70)
+
+def get_coco_label_map(coco, class_names):
+    COCO_LABEL_MAP = {}
+
+    coco_cat_index_MAP = {}
+    for index, cat in coco.cats.items():
+        if cat['name'] == '_background_':
+            continue
+        coco_cat_index_MAP[cat['name']] = index
+
+    for index, class_name in enumerate(class_names):
+        COCO_LABEL_MAP[coco_cat_index_MAP[class_name]] = index + 1
+    return COCO_LABEL_MAP
